5a5452da9887e9b4bbb4041ede5aa531e4d43d17
vayorix <vayorix.tech@gmail.com>
2026-06-02 09:58:44 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -2,10 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(
-
-  'mongodb+srv://shivarn:Shivarn@2021@dhruv.gfiy5jw.mongodb.net/?appName=Dhruv'
-);
+    await mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://shivarn:Shivarn%402021@dhruv.gfiy5jw.mongodb.net/?appName=Dhruv');
     console.log('Connected to MongoDB');
   } catch (err) {
     console.error('MongoDB connection error:', err);
